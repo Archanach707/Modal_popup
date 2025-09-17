cf198a9ad5634d4a4701e5481eb4268ecaeaf25a
Archanach707 <54771334+Archanach707@users.noreply.github.com>
2025-09-17 17:54:16 +0530

    two ways we are created model box the second approch this absolute good

---
+++ script2.js
@@ -0,0 +1,21 @@
+const button = document.querySelector('button')
+const closeIcon = document.querySelector('.close-icon')
+const popupContainer = document.querySelector('.popup-container')
+const popup = document.querySelector('.popup')
+
+
+button.addEventListener('click', () => {
+  popupContainer.classList.add('popup-open')
+})
+
+closeIcon.addEventListener('click', () => {
+  popupContainer.classList.remove('popup-open')
+})
+
+popupContainer.addEventListener('click', () => {
+  popupContainer.classList.remove('popup-open')
+})
+
+popup.addEventListener('click', (e) => {
+  e.stopPropagation()
+})
